6ad7192c0c596fc6870befa7653b5362db29d535
Sagardussa <92858280+Sagardussa@users.noreply.github.com>
2022-05-24 16:31:27 +0530

    ok

--- empprofile/src/app/app.module.ts
+++ empprofile/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmplistComponent } from './component/emplist/emplist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmplistComponent
   ],
   imports: [
     BrowserModule,
